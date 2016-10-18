a5c4f61669b9b6d0fa48c37199cbd2332fea4585
gunendu <gunendu@gmail.com>
2016-10-18 11:52:55 +0530

    ask location

--- index.js
+++ index.js
@@ -190,9 +190,27 @@ function receivedPostback(event) {
   console.log("Received postback for user %d and page %d with payload '%s' " +
     "at %d", senderID, recipientID, payload, timeOfPostback);
 
+  if(payload.indexOf("START_ORDER") > -1) {
+     var messageData = askLocation();
+     callSendAPI(messageData);
+  }
   // When a postback is called, we'll send a message back to the sender to
   // let them know it was successful
   sendTextMessage(senderID, "Postback called");
+}
+
+function askLocation() {
+  var messageData = {};
+  var recipient = {};
+  var message = {};
+  var quick_replies = [];
+  recipient.id = senderID;
+  message.text = "Please share your location:";
+  quick_replies.push({"content_type":"location"});
+  message.quick_replies = quick_replies;
+  messageData.recipient = recipient;
+  messageData.message = message;
+  return messageData;
 }
 
 function callUberApi(lat,long,senderID){
